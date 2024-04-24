edc06e27b8a109e78b3882caa492a6098429e727
DanZlotnikov <danzlotnikov@gmail.com>
2024-04-24 16:37:06 +0300

    c

---
+++ src/image_completion/image_completion.py
@@ -0,0 +1,5 @@
+from .fourier import *
+
+def complete_image(image):
+    fourier_transform('a')
+
--- src/main.py
+++ src/main.py
@@ -1,2 +1,4 @@
+from src.image_completion import image_completion
+
 if __name__ == '__main__':
-    print('hello')
+    image_completion.complete_image('a')
