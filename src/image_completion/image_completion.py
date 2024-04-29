c27c7dbd6b54af55abee6374865537135dcee9cd
DanZlotnikov <danzlotnikov@gmail.com>
2024-04-29 17:41:36 +0300

    divide to 4

--- src/image_completion/image_completion.py
+++ src/image_completion/image_completion.py
@@ -1,32 +1,10 @@
-import numpy as np
-import matplotlib.pyplot as plt
+# image_processing.py
 
 
-def fourier_predict_polygon(image):
-    # Create a mask to ignore points inside the polygon
-    mask = np.ones_like(image.gray_img, dtype=bool)
-    if image.polygon_closed:
-        polygon_points = np.array(image.clicked_points)
-        if len(polygon_points) > 0:  # Check if polygon_points is not empty
-            min_x, min_y = polygon_points.min(axis=0)
-            max_x, max_y = polygon_points.max(axis=0)
-            for i in range(int(min_y), int(max_y) + 1):
-                for j in range(int(min_x), int(max_x) + 1):
-                    if image.point_inside_polygon(j, i, polygon_points):
-                        mask[i, j] = False
+def divide_into_subimages(image):
+    M = image.shape[0] // 2
+    N = image.shape[1] // 2
+    subimages = [image[x:x+M, y:y+N] for x in range(0, image.shape[0], M) for y in range(0, image.shape[1], N)]
 
-    # Apply Fourier transform
-    f_transform = np.fft.fft2(image.gray_img * mask)
-    f_shift = np.fft.fftshift(f_transform)
-    magnitude_spectrum = 20 * np.log(np.abs(f_shift))
 
-    # Apply inverse Fourier transform
-    f_shift_back = np.fft.ifftshift(f_shift)
-    img_back = np.fft.ifft2(f_shift_back)
-    img_back = np.abs(img_back)
-
-    # Plot the predicted values inside the polygon
-    plt.imshow(img_back, cmap='gray')
-    plt.axis('off')
-    plt.title('Predicted Values Inside Polygon')
-    plt.show()
+    return subimages
--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
 from src.image import Image
 from src.image_completion.image_completion import *
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     image = Image('./nadal.jpg')
     image.plot()
-    fourier_predict_polygon(image)
+
+    # Call the function to divide the image into sub-images
+    subimages = divide_into_subimages(image.img)
+
+    # Process the sub-images further if needed
+    for i, subimage in enumerate(subimages):
+        # Perform operations on each sub-image
+        # For example, you can display each sub-image
+        plt.subplot(1, len(subimages), i + 1)
+        plt.imshow(subimage, cmap='gray')
+        plt.axis('off')
+    plt.show()
