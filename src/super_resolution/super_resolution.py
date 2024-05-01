95196fb74a3abaafdae3b55f7b869959c7f2338b
brahan-hub <brahan@post.bgu.ac.il>
2024-05-01 19:49:01 +0300

    maybe done?

--- src/super_resolution/super_resolution.py
+++ src/super_resolution/super_resolution.py
@@ -3,35 +3,33 @@ import math
 import numpy as np
 from scipy.ndimage import gaussian_filter
 import cv2
+from scipy.signal import convolve2d
 
 
-def dist(x,y,thres): # calc the distance between pixel values and set the confidence accordingly
-    # need to set it in a gaussian way
-    d= np.linalg.norm(x-y)
-    if d > thres:
-        return 0.9
-    else:
-        return 0.01
-
-def calc_initial_confidence(scaled_image,base_pixels):
-    initial_confidence = np.ones((scaled_image.shape[0],scaled_image.shape[1],21))
-    h,w = scaled_image.shape
-    interpolation_idx = 10
+def calc_initial_confidence(scaled_image_shape,base_pixels,n_buckets):
+    initial_confidence = np.ones((scaled_image_shape[0],scaled_image_shape[1],2*n_buckets+1))
+    h,w = scaled_image_shape
+    interpolation_idx = n_buckets
     for i in range(h):
         for j in range(w):
-            for k in enumerate(initial_confidence[i,j]):
-                distance = abs(k-interpolation_idx)
-                sigma = distance/math.sqrt(-2* np.log(0.8))
+            for k,v in enumerate(initial_confidence[i,j]):
+
                 if (i,j) in base_pixels:
-                    if k==10:
+                    if k==n_buckets:
                         initial_confidence[i,j,k] = 0.99
                     else:
                         initial_confidence[i, j,k] = 0.001
                 else:
-                    initial_confidence[i,j,k] = 0.8 * max.exp(-0.5 * (distance/sigma)**2)
+
+                    distance = abs(k - interpolation_idx)
+                    if distance != 0:
+                        sigma = distance / math.sqrt(-2 * np.log(0.8))
+                    else:
+                        sigma = 2
+                    initial_confidence[i,j,k] = 0.8 * np.exp(-0.5 * (distance/sigma)**2)
 
     return initial_confidence
-def upscale(image, scale_factor,thres):
+def upscale(image, scale_factor):
     """
     upscales the image by the scale factor using interpoltion to assign the new values to the upscaled image(initial labels
     :param image:
@@ -44,10 +42,7 @@ def upscale(image, scale_factor,thres):
     n_img = np.resize(image,( height*scale_factor,width*scale_factor))
     n_image = np.zeros_like(n_img, dtype=np.uint8)
     upscaled_image = np.zeros_like(n_img, dtype=np.uint8)
-
-    # initial_confidence = np.zeros_like(n_img, dtype=np.float32)
     base_pixels = {}
-    # initial_labels = np.zeros_like(n_img, dtype=np.uint8)
     for y in range(height):
         for x in range(width):
             for dy in range(scale_factor):
@@ -65,54 +60,63 @@ def upscale(image, scale_factor,thres):
                                 (1 - weight_x) * weight_y * image[min(y + 1, height - 1), x] +
                                 weight_x * weight_y * image[min(y + 1, height - 1), min(x + 1, width - 1)]
                         )
-                    # initial_labels[y,x] = image[y, x] # init the labels to the base_pixels values
-
 
     return upscaled_image, base_pixels
-#
-#
-def gaussian_kernel(image_shape, sigma):
-    # kernel_size = int(2 * np.ceil(2 * sigma) + 1)
-
-    kernel = gaussian_filter(np.zeros(image_shape), sigma=sigma)
-
-    return kernel / np.sum(kernel)
-#
-
-def calc_supp(kernel,curr_conf):
-    pass
 
 
-def update_confidence(image, confidence, support):
-    next_confidence = np.zeros_like(confidence)
+def calc_supp(kernel, curr_conf,n_buckets):
+    return np.array([convolve2d(curr_conf[:, :, label], kernel, mode='same') for label in
+                     range(2*n_buckets+1)]).transpose(1, 2, 0)
+
+
+def update_confidence(shape, confidence, support):
+    next_confidence = np.ones_like(confidence)
     confidence_sum = np.zeros_like(confidence)
-    height, width = image.shape
+    height, width = shape
     for i in range(height):
         for j in range(width):
             next_confidence[i][j] = confidence[i][j] * support[i][j]
             confidence_sum[i][j] += next_confidence[i][j]
-    return next_confidence
+    return next_confidence/ confidence_sum , np.sum(confidence_sum)
 
 
-def update_label(conff):
-    pass
+def calc_label(scaled_img,conff,n_buckets):
+    final_img = scaled_img.copy().astype(np.uint8)
+    optimal_indices = (np.argmax(conff, axis=-1) - n_buckets).astype(np.uint8)
+    final_img-=optimal_indices
+
+    return final_img
 
 
-def relaxation_labeling(image,scale_factor,epsilon):
-    scaled_image,base_pixels = upscale(image, scale_factor, 0.5)
+def relaxation_labeling(image,scale_factor,n_buckets,epsilon):
+    scaled_image,base_pixels = upscale(image, scale_factor)
     # # ____
-    kernel = gaussian_kernel(scaled_image.shape, scale_factor)
-    curr_conf = calc_initial_confidence(scaled_image,base_pixels)
+    # kernel =  np.ones((5, 5))
+    # linear kernel:
+    kernel = np.array([[0, 0, 1, 0, 0],
+                       [0, 1, 1, 1, 0],
+                       [1, 1, 1, 1, 1],
+                       [0, 1, 1, 1, 0],
+                       [0, 0, 1, 0, 0]])
+    curr_conf = calc_initial_confidence(scaled_image.shape,base_pixels,n_buckets)
+    print(curr_conf)
     k = 0  # counts the iteration number
+    current_change =1
     while True:
-        support = calc_supp(kernel,curr_conf)
-        next_conf = update_confidence(scaled_image,curr_conf, support)
-        diff = np.linalg.norm(curr_conf.values() - next_conf.values()) #TODO: need to think about avg measurement?
-        curr_conf = next_conf
+        print("relaxation iter:", k)
+        support = calc_supp(kernel,curr_conf,n_buckets)
+        # support = calc_support(scaled_image, list(range(-10,11,1)),curr_conf, kernel)
+        next_conf,next_change = update_confidence(scaled_image.shape,curr_conf, support)
+        diff = np.linalg.norm(curr_conf - next_conf) #TODO: need to think about avg measurement?
         k += 1
-        if diff < epsilon:
+        print(diff)
+        if  diff< epsilon: #abs(current_change - next_change)
             break
-    return update_label(next_conf)
+        else:
+          curr_conf = next_conf
+          current_change = next_change
+    # print(next_conf)
+    return calc_label(scaled_image,next_conf,n_buckets)
 
 
 
@@ -124,10 +128,8 @@ def upsample_image(input_image, target_resolution):
     h,w,c = input_image.shape
     DOUBLE = (4*w,4*h)
 
-    # Get the input image dimensions
     height, width, channels = input_image.shape
 
-    # Define the interpolation method
     interpolation_method = cv2.INTER_CUBIC
 
     # Upscale the image based on the target resolution
@@ -151,30 +153,33 @@ def upsample_image(input_image, target_resolution):
 
 if __name__ == '__main__':
 
-    input_image_path = "nadal.jpg"
-    target_resolution = "QHD"
+    input_image_path = "lenna.png"
+
     # Load the input image
     input_image = cv2.imread(input_image_path)
+    # image after downsample to 200X200
     input_image = cv2.resize(input_image, (200,200), interpolation=cv2.INTER_AREA)
-    output1_image_path = f"downsampled_{target_resolution.lower()}_image.jpg"
+    output1_image_path = f"downsampled_lenna_image.jpg"
     cv2.imwrite(output1_image_path, input_image)
 
-    # Upsample the image
+    #gray downsampled:
+    downsampled_gray_path = f"downsampled_gray_lenna_image.jpg"
     gray_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite(downsampled_gray_path, gray_image)
+
+    # image after upscaling by scale factor of 10:
     scale_factor = 10
-    good = relaxation_labeling(gray_image, scale_factor,0.5)
-    # upsampled_image = upsample_image(input_image, target_resolution)
-    # # Save the upsampled image
-    # if upsampled_image is not None:
-    #     output_image_path = f"upsampled_{target_resolution.lower()}_image.jpg"
-    #     cv2.imwrite(output_image_path, upsampled_image)
-    #     print(f"Upsampled image saved as {output_image_path}")
-    # else:
-    #     print("Failed to upsample the image.")
-    #
-    # denoised_image = relaxation_labeling(upsampled_image, max_iterations=15, compatibility_factor=0.8)
-    output_image_path = f"deno_upscale_{scale_factor}__image.jpg"
+    img_after_upscaling, _ = upscale(gray_image, scale_factor)
+    after_upscaling = f"upsampled_with_factor_{scale_factor}_noise_lenna_image.jpg"
+    cv2.imwrite(after_upscaling, img_after_upscaling)
+
+    # denoised upsampled image:
+    eps = 0.01
+    n_buckets = 20 # +val -val from the interpolation values, should be even number
+    good = relaxation_labeling(gray_image, scale_factor,n_buckets,eps)
+    output_image_path = f"denoised_upscaled_{scale_factor}_eps={eps}_lenna_image.jpg"
     cv2.imwrite(output_image_path, good)
+
     print("before:", gray_image.shape)
     print("after:", good.shape)
     # print("Denoised image saved as", output_image_path)
