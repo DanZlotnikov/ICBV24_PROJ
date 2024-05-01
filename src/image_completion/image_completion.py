a08cae81b24a5f438fa3f424a0532dfd0e44ac2c
DanZlotnikov <danzlotnikov@gmail.com>
2024-05-02 01:01:14 +0300

    fft_complete

--- src/image_completion/image_completion.py
+++ src/image_completion/image_completion.py
@@ -4,8 +4,14 @@ from src.image import Image
 import imageio
 import os
 
-def remove_rectangle(image_path, x, delta_x, y, delta_y):
-    full_path = "../uploads/"+image_path
+from src.image_completion.axis_fft_2d import AxisFFTPartial
+from src.image_completion.full_fft_2d import FullFFT2D
+
+uploads_directory = '../uploads'
+processed_directory = '../processed'
+
+def remove_rectangle(image_name, x, delta_x, y, delta_y):
+    full_path = uploads_directory + image_name
     image = Image(full_path)
     height, width = image.gray_img.shape
 
@@ -21,13 +27,31 @@ def remove_rectangle(image_path, x, delta_x, y, delta_y):
     # Remove the rectangle from the image
     modified_image[y:delta_y+y, x:delta_x+x] = 0
 
-
-    output_directory = '../uploads'
-    os.makedirs(output_directory, exist_ok=True)
-    imageio.imwrite(os.path.join(output_directory, image_path), modified_image)
+    os.makedirs(uploads_directory, exist_ok=True)
+    imageio.imwrite(full_path, modified_image)
 
     return modified_image
 
 
 def fft_complete(method, image_name, x, y, delta_x, delta_y):
-    pass
+    full_path = uploads_directory + image_name
+    image = cv2.imread(full_path, cv2.IMREAD_GRAYSCALE)
+    reconstructed_part_from_full = None
+
+    if method == 'Single':
+        model = FullFFT2D().fit(image)
+        reconstructed_part_from_full = model.predict(x, y, delta_x, delta_y)
+    elif method == 'Full':
+        model = AxisFFTPartial.fit(image)
+        reconstructed_part_from_full = model.predict(x, y, delta_x, delta_y)
+    elif method == 'TileFFT2D':
+        model = AxisFFTPartial.fit(image)
+        reconstructed_part_from_full = model.predict(x, y, delta_x, delta_y)
+
+    completed_image = np.copy(image)
+    completed_image[y:y+delta_y, x:x+delta_x] = reconstructed_part_from_full
+
+    new_path = os.path.join(processed_directory, image_name)
+    imageio.imwrite(new_path, completed_image)
+    return True
+
