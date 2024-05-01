634ac8cd27082105048c17db7c2b31f8d0abcda1
DanZlotnikov <danzlotnikov@gmail.com>
2024-05-01 19:21:03 +0300

    c

--- src/image_completion/image_completion.py
+++ src/image_completion/image_completion.py
@@ -1,18 +1,18 @@
-# image_processing.py
+import numpy as np
 
 
-def divide_into_subimages(image):
-    gray = image.gray_img
-    x_markers = [0, int(image.rect_points[0][0]), int(image.rect_points[1][0]), gray.shape[1]]
-    y_markers = [0, int(image.rect_points[0][1]), int(image.rect_points[1][1]), gray.shape[0]]
+def remove_rectangle(image, x, y, delta_x, delta_y):
+    height, width = image.gray_img.shape
 
-    sub_images = []
+    # Ensure the rectangle coordinates are within bounds
+    x = max(0, min(x, width - 1))
+    y = max(0, min(y, height - 1))
+    delta_x = max(0, min(delta_x, width - x))
+    delta_y = max(0, min(delta_y, height - y))
 
-    # Loop through rows and columns to crop the image into 9 rectangles
-    for i in range(0, len(y_markers) - 1):
-        for j in range(0, len(x_markers) - 1):
-            cropped = image.gray_img[y_markers[j]:y_markers[j + 1], x_markers[i]:x_markers[i + 1]]
-            sub_images.append(cropped)
+    # Create a copy of the image
+    modified_image = np.copy(image.gray_img)
 
-    return sub_images
-
+    # Remove the rectangle from the image
+    modified_image[y:y + delta_y, x:x + delta_x] = 0
+    return modified_image
--- src/main.py
+++ src/main.py
@@ -1,23 +1,14 @@
 from src.image import Image
-from src.image_completion.image_completion import *
 import matplotlib.pyplot as plt
+from src.image_completion import *
+
 
 if __name__ == '__main__':
     image = Image('./nadal.jpg')
-    image.plot()
-
-    # Call the function to divide the image into sub-images
-    subimages = divide_into_subimages(image)
-
-    # Process the sub-images further if needed
-    num_subimages = len(subimages)
-    num_rows = 3
-    num_cols = num_subimages // num_rows
-
-    fig, axes = plt.subplots(num_rows, num_cols, figsize=(12, 8))
-    for i, subimage in enumerate(subimages):
-        row = i // num_cols
-        col = i % num_cols
-        axes[col, row].imshow(subimage, cmap='gray')
-        axes[col, row].axis('off')
+    image.display()
+    modified = remove_rectangle(image, 100, 100, 500, 500)
+    plt.imshow(modified, cmap='gray')
+    plt.axis('off')  # Turn off axis
     plt.show()
+
+
